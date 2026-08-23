b7399fadb0340e63f8c8b004d10712c8dd73c23d
Shubham Hadawle <shubham.hadawle@outlook.com>
2026-08-22 20:12:07 -0400

    Add: insert-new-interval - submission-2

---
+++ Data Structures & Algorithms/insert-new-interval/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        # Greedy Approach
+        result = []
+
+        for curr in range(0, len(intervals)):
+            # CASE 1: 'newInterval' falls before 'currInterval'
+            if newInterval[1] < intervals[curr][0]:
+                result.append(newInterval)
+                result = result + intervals[curr:]
+                return result
+
+            # CASE 2: 'newInterval' falls after 'currInterval'
+            elif intervals[curr][1] < newInterval[0]:
+                result.append(intervals[curr])
+
+            # CASE 3: Both overlap
+            else:
+                newInterval[0] = min(newInterval[0], intervals[curr][0])
+                newInterval[1] = max(newInterval[1], intervals[curr][1])
+
+        # CASE 4: 'newInterval' falls/ends after all iterated intervals
+        result.append(newInterval)
+        return result
